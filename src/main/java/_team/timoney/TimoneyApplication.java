30f7f4a0aec56517609ccd63afdf23eba96fff33
saaad99 <ehdgnstla@naver.com>
2025-07-07 17:02:31 +0900

    프로젝트 초기화

---
+++ src/main/java/_team/timoney/TimoneyApplication.java
@@ -0,0 +1,13 @@
+package _team.timoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimoneyApplication.class, args);
+	}
+
+}
