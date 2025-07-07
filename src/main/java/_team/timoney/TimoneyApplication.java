b9e6d7cc423d78d715f2ce865dcbfe87646cb1ce
saaad99 <ehdgnstla@naver.com>
2025-07-07 18:29:52 +0900

    Chore: 로그인 페이지 뜨지 않게 수정

--- src/main/java/_team/timoney/TimoneyApplication.java
+++ src/main/java/_team/timoney/TimoneyApplication.java
@@ -2,8 +2,9 @@ package _team.timoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TimoneyApplication {
 
 	public static void main(String[] args) {
