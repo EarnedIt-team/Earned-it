945307bc34c53d89bfb1eb374ca9d6767d25f84d
yoorym-kim <twilight99me@gmail.com>
2025-07-16 17:42:47 +0900

    feat : EmailToken 엔티티 정의

---
+++ src/main/java/_team/earnedit/entity/EmailToken.java
@@ -0,0 +1,35 @@
+package _team.earnedit.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class EmailToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 5, nullable = false, unique = true)
+    private String token;  // 5자리 숫자 (랜덤)
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private LocalDateTime expiredAt;
+
+    @Column(nullable = false)
+    private boolean isVerified = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
