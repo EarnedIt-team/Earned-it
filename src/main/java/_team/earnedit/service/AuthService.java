99be4f7bf07049de67bf5f211900f9f3fa8fef00
yoorym-kim <twilight99me@gmail.com>
2025-07-16 17:26:53 +0900

    feat : SignUp 서비스메서드에서 요청/응답 DTO 사용

--- src/main/java/_team/earnedit/service/AuthService.java
+++ src/main/java/_team/earnedit/service/AuthService.java
@@ -1,5 +1,7 @@
 package _team.earnedit.service;
 
+import _team.earnedit.dto.auth.SignUpRequestDto;
+import _team.earnedit.dto.auth.SignUpResponseDto;
 import _team.earnedit.entity.User;
 import _team.earnedit.global.ErrorCode;
 import _team.earnedit.global.exception.user.UserException;
@@ -7,28 +9,33 @@ import _team.earnedit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static _team.earnedit.entity.User.Status.ACTIVE;
+
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final UserRepository userRepository;
 
-    public User signup(String email, String password, String nickname) {
+    public SignUpResponseDto signUp(SignUpRequestDto requestDto) {
+        String email = requestDto.getEmail();
+        String password = requestDto.getPassword();
+        String nickname = requestDto.getNickname();
 
         if (userRepository.existsByEmail(email)) {
             throw new UserException(ErrorCode.EMAIL_ALREADY_EXISTED);
         }
 
-        User user = User.builder()
-                .email(email)
-                .password(password)
-                .nickname(nickname)
-                .status(User.Status.ACTIVE)
-                .provider(User.Provider.LOCAL)
-                .isDarkMode(false)
-                .isPublic(false)
-                .build();
+        User user = userRepository.save(
+                User.builder()
+                        .email(email)
+                        .password(password)
+                        .nickname(nickname)
+                        .provider(User.Provider.LOCAL)
+                        .status(ACTIVE)
+                        .build()
+        );
 
-        return userRepository.save(user);
+        return new SignUpResponseDto(user.getId(), user.getEmail(), user.getNickname());
     }
 }
