82090e188ccc6e89656a0f55d2a48102b6dbe092
yoorym-kim <twilight99me@gmail.com>
2025-07-21 14:52:03 +0900

    feat : SignUpRequestDto에 @Builder.Default 설정

--- src/main/java/_team/earnedit/dto/auth/SignUpRequestDto.java
+++ src/main/java/_team/earnedit/dto/auth/SignUpRequestDto.java
@@ -1,10 +1,21 @@
 package _team.earnedit.dto.auth;
 
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class SignUpRequestDto {
+
     private String email;
+
     private String password;
+
     private String nickname;
+
+    @Builder.Default
+    private Boolean isDarkMode = false;
+
+    @Builder.Default
+    private Boolean isPublic = false;
 }
