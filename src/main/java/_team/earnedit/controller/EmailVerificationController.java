aed53d78f14073b0d388094140b024698390ce4e
yoorym-kim <twilight99me@gmail.com>
2025-07-21 15:35:30 +0900

    feat : 이메일 인증 요청 컨트롤러메서드 구현

--- src/main/java/_team/earnedit/controller/EmailVerificationController.java
+++ src/main/java/_team/earnedit/controller/EmailVerificationController.java
@@ -1,4 +1,21 @@
 package _team.earnedit.controller;
 
+import _team.earnedit.service.EmailVerificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth/email")
 public class EmailVerificationController {
+
+    private final EmailVerificationService emailVerificationService;
+
+    // 이메일 인증 요청 (검증코드 포함된 이메일 전송)
+    @PostMapping("/send")
+    public ResponseEntity<String> sendEmailVerification(@RequestParam String email) {
+        emailVerificationService.sendEmailVerification(email);
+        return ResponseEntity.ok("인증 이메일이 전송되었습니다.");
+    }
 }
