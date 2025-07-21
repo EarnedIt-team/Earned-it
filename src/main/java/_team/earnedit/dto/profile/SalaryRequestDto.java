b3672430bc9460f0f880af63ad45538fb8a7e136
yoorym-kim <twilight99me@gmail.com>
2025-07-21 20:23:35 +0900

    feat : Salary 요청/응답 dto 생성

---
+++ src/main/java/_team/earnedit/dto/profile/SalaryRequestDto.java
@@ -0,0 +1,12 @@
+package _team.earnedit.dto.profile;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class SalaryRequestDto {
+
+    private Long salary;
+
+}
---
+++ src/main/java/_team/earnedit/dto/profile/SalaryResponseDto.java
@@ -0,0 +1,4 @@
+package _team.earnedit.dto.profile;
+
+public class SalaryResponseDto {
+}
