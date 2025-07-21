b295b8e8bde6d445e4f9abad046056506d4aecb9
yoorym-kim <twilight99me@gmail.com>
2025-07-21 20:36:22 +0900

    feat : updateSalary 서비스메서드 구현

---
+++ src/main/java/_team/earnedit/service/ProfileService.java
@@ -0,0 +1,29 @@
+package _team.earnedit.service;
+
+import _team.earnedit.dto.profile.SalaryRequestDto;
+import _team.earnedit.dto.profile.SalaryResponseDto;
+import _team.earnedit.entity.Salary;
+import _team.earnedit.repository.SalaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final SalaryRepository salaryRepository;
+
+    public SalaryResponseDto updateSalary(SalaryRequestDto requestDto) {
+        Long amount = requestDto.getAmount();
+        Salary salary = salaryRepository.save(
+                Salary.builder()
+                        .amount(amount)
+                        .build()
+        );
+        return new SalaryResponseDto();
+    }
+
+//    public SalaryResponseDto getSalary() {
+//
+//    }
+}
