d911d121d1d0a7b698a4210c2197e63b8d845afd
yoorym-kim <twilight99me@gmail.com>
2025-07-21 20:14:11 +0900

    feat : Salary 엔티티 정의

---
+++ src/main/java/_team/earnedit/entity/Salary.java
@@ -0,0 +1,46 @@
+package _team.earnedit.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "salary")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Salary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SalaryType type;
+
+    private Boolean severance;
+
+    @Column(nullable = false)
+    private Long salary;
+
+    private Integer dependentCount;
+
+    private Long taxExemptAmount;
+
+    @Column(nullable = false)
+    private Boolean tax = false;
+
+    @Column(nullable = false)
+    private Double salaryPerSec;
+
+    public enum SalaryType {
+        MONTH,
+        YEAR,
+        NONE
+    }
+}
