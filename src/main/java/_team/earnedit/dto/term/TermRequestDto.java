05b18f5b34c446421439124e250b7cf3bbecd5e1
yoorym-kim <twilight99me@gmail.com>
2025-07-21 17:57:25 +0900

    feat : TermRequestDto 정보 추가

--- src/main/java/_team/earnedit/dto/term/TermRequestDto.java
+++ src/main/java/_team/earnedit/dto/term/TermRequestDto.java
@@ -1,4 +1,15 @@
 package _team.earnedit.dto.term;
 
+import _team.earnedit.entity.Term;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class TermRequestDto {
+
+    private Term.Type type;
+
+    private boolean isChecked;
+
 }
