5de029aa5ea9d5219744b6e903d5df1ff072a34c
saaad99 <ehdgnstla@naver.com>
2025-07-22 18:01:05 +0900

    feat: s3 업로드 컨트롤러 추가 - 에러 코드 추가 - 적절한 예외를 던지도록 수정 - 컨트롤러의 @Slf4j 어노테이션 제거

--- src/main/java/_team/earnedit/controller/FileUploadController.java
+++ src/main/java/_team/earnedit/controller/FileUploadController.java
@@ -1,11 +1,10 @@
 package _team.earnedit.controller;
 
+import _team.earnedit.global.ApiResponse;
+import _team.earnedit.service.FileUploadService;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,30 +12,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/upload")
-@Slf4j
 public class FileUploadController {
-    private final AmazonS3Client s3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private final FileUploadService fileUploadService;
+
 
     @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileName = file.getOriginalFilename();
-            String fileUrl = "https://" + bucket + "/test" + fileName;
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(file.getContentType());
-            metadata.setContentLength(file.getSize());
-            s3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
-            return ResponseEntity.ok(fileUrl);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    public ResponseEntity<ApiResponse<String>> uploadFile(@RequestParam("file") MultipartFile file) {
+        String url = fileUploadService.uploadFile(file);
+
+        return ResponseEntity.ok(ApiResponse.success("file successfully uploaded", url));
     }
 }
