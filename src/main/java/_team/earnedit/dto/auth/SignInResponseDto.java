2e56388fa62809033cfa79b5b93e01704544936d
yoorym-kim <twilight99me@gmail.com>
2025-07-22 17:35:32 +0900

    feat : SignInResponseDto 생성

---
+++ src/main/java/_team/earnedit/dto/auth/SignInResponseDto.java
@@ -0,0 +1,12 @@
+package _team.earnedit.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignInResponseDto {
+    private String accessToken;
+    private String refreshToken;
+    private Long userId;
+}
