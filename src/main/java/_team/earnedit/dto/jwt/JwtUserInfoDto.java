6c133ecca8f730272990d64086da8df7c786241d
yoorym-kim <twilight99me@gmail.com>
2025-07-22 17:34:32 +0900

    feat : JwtUserInfoDto, JwtUtil, JwtAuthFilter, JwtAuthenticationEntryPoint 추가

---
+++ src/main/java/_team/earnedit/dto/jwt/JwtUserInfoDto.java
@@ -0,0 +1,10 @@
+package _team.earnedit.dto.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JwtUserInfoDto {
+    private Long userId;
+}
