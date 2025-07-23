fb2a689903d831b0e0cb774ff5c8801d4a0457d6
yoorym-kim <twilight99me@gmail.com>
2025-07-24 00:33:43 +0900

    feat : saveSalary에서 @AuthenticationPrincipal로 userId 꺼내기

--- src/main/java/_team/earnedit/controller/ProfileController.java
+++ src/main/java/_team/earnedit/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package _team.earnedit.controller;
 
+import _team.earnedit.dto.jwt.JwtUserInfoDto;
 import _team.earnedit.dto.profile.SalaryRequestDto;
 import _team.earnedit.dto.profile.SalaryResponseDto;
 import _team.earnedit.global.ApiResponse;
@@ -7,6 +8,7 @@ import _team.earnedit.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +18,10 @@ public class ProfileController {
     private final ProfileService profileService;
 
     @PostMapping("/salary")
-    public ResponseEntity<ApiResponse<SalaryResponseDto>> saveSalary(@RequestParam long userId, @RequestBody SalaryRequestDto requestDto) {
-        SalaryResponseDto responseDto = profileService.updateSalary(userId, requestDto);
+    public ResponseEntity<ApiResponse<SalaryResponseDto>> saveSalary(
+            @AuthenticationPrincipal JwtUserInfoDto userInfo,
+            @RequestBody SalaryRequestDto requestDto) {
+        SalaryResponseDto responseDto = profileService.updateSalary(userInfo.getUserId(), requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("수익 정보를 업데이트했습니다",responseDto));
     }
