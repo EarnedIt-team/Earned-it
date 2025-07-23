8eae0bd8bc2c062310444a609309c0ee954ef9cf
yoorym-kim <twilight99me@gmail.com>
2025-07-23 23:48:15 +0900

    feat : RefreshResponseDto 생성

---
+++ src/main/java/_team/earnedit/dto/auth/RefreshResponseDto.java
@@ -0,0 +1,10 @@
+package _team.earnedit.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RefreshResponseDto {
+    private String accessToken;
+}
