83f99193d8b67239da33ce2c4ca3bae7e8ff9457
yoorym-kim <twilight99me@gmail.com>
2025-07-24 01:18:12 +0900

    feat : saveSalary 기능 @AuthenticationPrincipal 사용으로 변경

--- src/main/java/_team/earnedit/controller/ProfileController.java
+++ src/main/java/_team/earnedit/controller/ProfileController.java
@@ -1,12 +1,16 @@
 package _team.earnedit.controller;
 
+import _team.earnedit.dto.jwt.JwtUserInfoDto;
 import _team.earnedit.dto.profile.SalaryRequestDto;
 import _team.earnedit.dto.profile.SalaryResponseDto;
 import _team.earnedit.global.ApiResponse;
 import _team.earnedit.service.ProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,9 +19,14 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
     private final ProfileService profileService;
 
+    @Operation(
+            security = {@SecurityRequirement(name = "bearer-key")}
+    )
     @PostMapping("/salary")
-    public ResponseEntity<ApiResponse<SalaryResponseDto>> saveSalary(@RequestParam long userId, @RequestBody SalaryRequestDto requestDto) {
-        SalaryResponseDto responseDto = profileService.updateSalary(userId, requestDto);
+    public ResponseEntity<ApiResponse<SalaryResponseDto>> saveSalary(
+            @AuthenticationPrincipal JwtUserInfoDto userInfo,
+            @RequestBody SalaryRequestDto requestDto) {
+        SalaryResponseDto responseDto = profileService.updateSalary(userInfo.getUserId(), requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("수익 정보를 업데이트했습니다",responseDto));
     }
