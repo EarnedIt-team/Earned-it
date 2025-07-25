8bd810da8eff20ab274e983a244956994e286d00
saaad99 <ehdgnstla@naver.com>
2025-07-25 14:34:40 +0900

    feat: wishAddResponse @Getter 추가

--- src/main/java/_team/earnedit/dto/wish/WishAddResponse.java
+++ src/main/java/_team/earnedit/dto/wish/WishAddResponse.java
@@ -1,10 +1,12 @@
 package _team.earnedit.dto.wish;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class WishAddResponse {
 
     private long wishId;
