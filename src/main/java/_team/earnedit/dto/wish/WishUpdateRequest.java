b03786cbd1bdc38521542d7c3b08c232f95a1751
saaad99 <ehdgnstla@naver.com>
2025-07-25 16:36:17 +0900

    feat: wish Update Request 유효성 검증 추가

--- src/main/java/_team/earnedit/dto/wish/WishUpdateRequest.java
+++ src/main/java/_team/earnedit/dto/wish/WishUpdateRequest.java
@@ -1,12 +1,21 @@
 package _team.earnedit.dto.wish;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class WishUpdateRequest {
+
+    @NotBlank(message = "위시 이름은 공백값이 불가능합니다.")
     private String name;
+
     private String vendor;
-    private String itemImage;
+
+    @NotNull(message = "가격은 필수입니다.")
     private int price;
+
+    @NotBlank(message = "상품 이미지는 반드시 있어야합니다." )
+    private String itemImage;
     private String url;
 }
