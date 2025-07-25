eed29e5b72f91ff1f4b8fd7320e5de58806f94b5
yoorym-kim <twilight99me@gmail.com>
2025-07-25 17:54:36 +0900

    feat : signInWithKakao 컨트롤러 메서드 구현

--- src/main/java/_team/earnedit/controller/AuthController.java
+++ src/main/java/_team/earnedit/controller/AuthController.java
@@ -1,6 +1,7 @@
 package _team.earnedit.controller;
 
 import _team.earnedit.dto.auth.*;
+import _team.earnedit.dto.socialLogin.KakaoSignInRequestDto;
 import _team.earnedit.global.ApiResponse;
 import _team.earnedit.service.AuthService;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -18,15 +19,28 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<ApiResponse<SignUpResponseDto>> signup(@RequestBody @Valid SignUpRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<SignUpResponseDto>> signup(
+            @RequestBody @Valid SignUpRequestDto requestDto
+    ) {
         SignUpResponseDto responseDto = authService.signUp(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("회원가입이 완료되었습니다.", responseDto));
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<ApiResponse<SignInResponseDto>> signIn(@RequestBody SignInRequestDto requestDto) {
+    public ResponseEntity<ApiResponse<SignInResponseDto>> signIn(
+            @RequestBody SignInRequestDto requestDto
+    ) {
         SignInResponseDto responseDto = authService.signIn(requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success("로그인이 완료되었습니다.", responseDto));
+    }
+
+    @PostMapping("/signin/kakao")
+    public ResponseEntity<ApiResponse<SignInResponseDto>> signInWithKakao(
+            @RequestBody KakaoSignInRequestDto requestDto
+    ) {
+        SignInResponseDto responseDto = authService.signInWithKakao(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success("로그인이 완료되었습니다.", responseDto));
     }
