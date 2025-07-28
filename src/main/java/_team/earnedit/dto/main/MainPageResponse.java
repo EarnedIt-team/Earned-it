ddd37acb9af625f1c175a44cd0c7546b0daef3e6
saaad99 <ehdgnstla@naver.com>
2025-07-28 18:36:49 +0900

    feat: 메인 페이지 정보 조회 API 구현

---
+++ src/main/java/_team/earnedit/dto/main/MainPageResponse.java
@@ -0,0 +1,22 @@
+package _team.earnedit.dto.main;
+
+import _team.earnedit.dto.wish.WishListResponse;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class MainPageResponse {
+
+    private UserInfo userInfo;
+    private List<WishListResponse> starWishes;
+
+    @Getter
+    @Builder
+    public static class UserInfo {
+        private double amountPerSec;
+        private boolean hasSalary;
+    }
+}
