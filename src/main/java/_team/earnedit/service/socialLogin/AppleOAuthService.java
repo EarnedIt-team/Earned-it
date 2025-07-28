7fce807db5f8b2114086c61baa394cc5df63e7af
yoorym-kim <twilight99me@gmail.com>
2025-07-28 15:36:09 +0900

    chore : AppleOAuthService 파일 추가

---
+++ src/main/java/_team/earnedit/service/socialLogin/AppleOAuthService.java
@@ -0,0 +1,9 @@
+package _team.earnedit.service.socialLogin;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppleOAuthService {
+}
