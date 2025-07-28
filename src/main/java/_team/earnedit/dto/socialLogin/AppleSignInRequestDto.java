7c641894c41b52f0cdeaef23d50824bc0ada0feb
yoorym-kim <twilight99me@gmail.com>
2025-07-28 18:27:46 +0900

    feat : AppleSignInRequestDto 생성

---
+++ src/main/java/_team/earnedit/dto/socialLogin/AppleSignInRequestDto.java
@@ -0,0 +1,12 @@
+package _team.earnedit.dto.socialLogin;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AppleSignInRequestDto {
+
+    private String idToken;
+
+}
