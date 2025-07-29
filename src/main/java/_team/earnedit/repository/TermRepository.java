962715423103262a6fdba035e3b12504562de4bb
yoorym-kim <twilight99me@gmail.com>
2025-07-29 16:23:07 +0900

    feat : TermRepository에 existsByUserIdAndType 추가

--- src/main/java/_team/earnedit/repository/TermRepository.java
+++ src/main/java/_team/earnedit/repository/TermRepository.java
@@ -1,7 +1,11 @@
 package _team.earnedit.repository;
 
 import _team.earnedit.entity.Term;
+import _team.earnedit.entity.Term.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TermRepository extends JpaRepository<Term, Long> {
+
+    boolean existsByUserIdAndType(Long userId, Type type);
+
 }
