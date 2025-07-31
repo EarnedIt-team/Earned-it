8bfe2c419a6cba47e26520200d64768c379416df
yoorym-kim <twilight99me@gmail.com>
2025-07-31 17:35:36 +0900

    feat : PasswordResetController 생성

---
+++ src/main/java/_team/earnedit/controller/PasswordResetController.java
@@ -0,0 +1,12 @@
+package _team.earnedit.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth/password")
+public class PasswordResetController {
+
+}
