3a65c3a8db20a68ae7aa5f0c633aa9167821f10d
saaad99 <ehdgnstla@naver.com>
2025-07-31 14:03:18 +0900

    ref: MainController 스웨거 문서화

--- src/main/java/_team/earnedit/controller/MainController.java
+++ src/main/java/_team/earnedit/controller/MainController.java
@@ -6,6 +6,7 @@ import _team.earnedit.global.ApiResponse;
 import _team.earnedit.service.MainService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/mainpage")
 @RequiredArgsConstructor
+@Tag(name = "MainPage API", description = "메인 페이지 정보 조회 API")
 public class MainController {
 
     private final MainService mainService;
