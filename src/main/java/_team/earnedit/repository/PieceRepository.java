729f5c8842844f3a7d2243697dc79ba5bc362de5
saaad99 <ehdgnstla@naver.com>
2025-07-31 21:24:01 +0900

    feat: 메인페이지 응답객체에 pieceInfo추가

--- src/main/java/_team/earnedit/repository/PieceRepository.java
+++ src/main/java/_team/earnedit/repository/PieceRepository.java
@@ -6,9 +6,11 @@ import _team.earnedit.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PieceRepository extends JpaRepository<Piece, Long> {
     List<Piece> user(User user);
     List<Piece> findByUserId(long userId);
     List<Piece> findByItemAndUser(Item item, User user);
+    Optional<Piece> findTopByUserIdOrderByCollectedAtDesc(Long userId);
 }
