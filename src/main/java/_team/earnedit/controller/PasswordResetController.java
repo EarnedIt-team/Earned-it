28e84b4d271ce0ab37aee0370dd50044e41363e5
yoorym-kim <twilight99me@gmail.com>
2025-07-31 17:39:21 +0900

    feat : SendResetEmail 컨트롤러메서드 구현

--- src/main/java/_team/earnedit/controller/PasswordResetController.java
+++ src/main/java/_team/earnedit/controller/PasswordResetController.java
@@ -1,12 +1,29 @@
 package _team.earnedit.controller;
 
+import _team.earnedit.dto.auth.PasswordResetRequestDto;
+import _team.earnedit.dto.auth.PasswordResetTokenVerifyRequestDto;
+import _team.earnedit.global.ApiResponse;
+import _team.earnedit.service.PasswordResetService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth/password")
 public class PasswordResetController {
+
+    private final PasswordResetService passwordResetService;
+
+    // 이메일 인증 코드 전송
+    @PostMapping("/email")
+    public ResponseEntity<ApiResponse<String>> sendResetEmail(
+            @RequestParam String email)
+    {
+        passwordResetService.sendPasswordResetEmail(email);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success("비밀번호 재설정 이메일이 전송되었습니다.", email));
+    }
 
 }
