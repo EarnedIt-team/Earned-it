9f71788dc9a3883f55a5193274f5484329ca63a6
saaad99 <ehdgnstla@naver.com>
2025-08-04 00:38:38 +0900

    doc: FileUploadController Swagger 문서화

--- src/main/java/_team/earnedit/controller/FileUploadController.java
+++ src/main/java/_team/earnedit/controller/FileUploadController.java
@@ -2,12 +2,13 @@ package _team.earnedit.controller;
 
 import _team.earnedit.global.ApiResponse;
 import _team.earnedit.service.FileUploadService;
-import com.amazonaws.services.s3.AmazonS3Client;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,9 +25,11 @@ public class FileUploadController {
     private final FileUploadService fileUploadService;
 
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "사진 업로드", description = "입력받은 사진 또는 파일을 S3에 업로드합니다.", security = {@SecurityRequirement(name = "bearer-key")})
-    public ResponseEntity<ApiResponse<String>> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<ApiResponse<String>> uploadFile(
+            @Parameter(description = "업로드할 이미지 파일", content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE))
+            @RequestParam("file") MultipartFile file) {
         String url = fileUploadService.uploadFile(file);
 
         return ResponseEntity.ok(ApiResponse.success("file successfully uploaded", url));
