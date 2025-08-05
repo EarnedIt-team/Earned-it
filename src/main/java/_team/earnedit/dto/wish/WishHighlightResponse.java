8dc3d228292f947a989133c0d48a26d9fe0cbb28
saaad99 <ehdgnstla@naver.com>
2025-08-05 14:56:29 +0900

    ref: 스웨거 작업 및 top3 응답객체명 수정

--- src/main/java/_team/earnedit/dto/wish/WishHighlightResponse.java
+++ src/main/java/_team/earnedit/dto/wish/WishHighlightResponse.java
@@ -11,14 +11,21 @@ import java.util.List;
 @Schema(description = "위시 하이라이트 응답 DTO")
 public class WishHighlightResponse {
 
+    @Schema(description = "위시 개수 정보")
     private WishInfo wishInfo;
-    private List<WishDetailResponse> wishDetailResponse;
 
+    @Schema(description = "최대 3개의 위시 목록")
+    private List<WishDetailResponse> wishHighlight;
 
     @Builder
     @Getter
     @Schema(description = "위시 개수 응답 DTO")
     public static class WishInfo {
+
+        @Schema(description = "현재 등록된 위시 개수", example = "23")
         private int currentWishCount;
+
+        @Schema(description = "등록 가능한 최대 위시 개수", example = "100")
+        private int limitWishCount;
     }
 }
