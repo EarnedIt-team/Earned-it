10a4d2d88bd71534066a24807bc3068d61f8b726
saaad99 <ehdgnstla@naver.com>
2025-08-05 16:45:44 +0900

    feat: 테마 추가

--- src/main/java/_team/earnedit/entity/Theme.java
+++ src/main/java/_team/earnedit/entity/Theme.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Theme {
     SWEET_AND_SOUR("새콤? 달콤!"),
-    CS_MUST_HAVE("컴공과 필수템");
+    CS_MUST_HAVE("컴공과 필수템"),
+    SMALL_HAPPINESS_INTERIOR("소확행 인테리어"),
+    SENSUAL_CHOCOLATE("감성 초콜릿 컬렉션"),
+    NOSTALGIA_PACKAGE("추억의 아이템");
 
     private final String displayName;
 
