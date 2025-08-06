2fe4cf442aebaace5cbebbba824270237238f41a
yoorym-kim <twilight99me@gmail.com>
2025-08-06 15:44:46 +0900

    feat : Profile Dto들 스웨거 코드 추가

--- src/main/java/_team/earnedit/dto/profile/ProfileImageRequestDto.java
+++ src/main/java/_team/earnedit/dto/profile/ProfileImageRequestDto.java
@@ -1,11 +1,14 @@
 package _team.earnedit.dto.profile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Schema(description = "프로필 이미지 업로드 DTO")
 public class ProfileImageRequestDto {
 
+    @Schema(description = "업로드할 프로필 이미지 파일", type = "string", format = "binary")
     private MultipartFile profileImage;
 
     public ProfileImageRequestDto(MultipartFile profileImage) {
--- src/main/java/_team/earnedit/dto/profile/ProfileInfoResponseDto.java
+++ src/main/java/_team/earnedit/dto/profile/ProfileInfoResponseDto.java
@@ -1,12 +1,20 @@
 package _team.earnedit.dto.profile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@Schema(description = "프로필 정보 응답 DTO")
 public class ProfileInfoResponseDto {
+
+    @Schema(description = "프로필 이미지 URL", example = "https://s3.../profile.png")
     private String profileImage;
+
+    @Schema(description = "사용자 닉네임", example = "요림짱")
     private String nickname;
+
+    @Schema(description = "월 수익 (단위: 원)", example = "4000000")
     private Long monthlySalary;
 }
