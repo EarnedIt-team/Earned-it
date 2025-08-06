d9ed7dae9b3b3658651795c0077445f2d1ea861c
yoorym-kim <twilight99me@gmail.com>
2025-08-06 15:34:17 +0900

    feat : ProfileController 불필요 import 삭제

--- src/main/java/_team/earnedit/controller/ProfileController.java
+++ src/main/java/_team/earnedit/controller/ProfileController.java
@@ -14,9 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
