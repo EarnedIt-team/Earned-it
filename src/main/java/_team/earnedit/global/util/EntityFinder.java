0f3b139502afd9f160308993f89526be582f58d3
saaad99 <ehdgnstla@naver.com>
2025-08-07 15:33:15 +0900

    ref: user, wish 예외 메서드 추가

--- src/main/java/_team/earnedit/global/util/EntityFinder.java
+++ src/main/java/_team/earnedit/global/util/EntityFinder.java
@@ -1,9 +1,12 @@
 package _team.earnedit.global.util;
 
 import _team.earnedit.entity.User;
+import _team.earnedit.entity.Wish;
 import _team.earnedit.global.ErrorCode;
 import _team.earnedit.global.exception.user.UserException;
+import _team.earnedit.global.exception.wish.WishException;
 import _team.earnedit.repository.UserRepository;
+import _team.earnedit.repository.WishRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +15,16 @@ import org.springframework.stereotype.Component;
 public class EntityFinder {
 
     private final UserRepository userRepository;
+    private final WishRepository wishRepository;
 
-    public User getUserOrThrow(Long id) {
-        return userRepository.findById(id)
+    public User getUserOrThrow(Long userId) {
+        return userRepository.findById(userId)
                 .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
     }
+
+    public Wish getWishOrThrow(Long wishId) {
+        return wishRepository.findById(wishId)
+                .orElseThrow(() -> new WishException(ErrorCode.WISH_NOT_FOUND));
+    }
+
 }
