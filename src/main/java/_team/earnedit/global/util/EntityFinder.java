7187f99d9b3ccd887c7dd5b63f27f97549c81ee2
saaad99 <ehdgnstla@naver.com>
2025-08-07 15:38:56 +0900

    ref: DailyCheckService 예외 메서드 호출하도록 수정

--- src/main/java/_team/earnedit/global/util/EntityFinder.java
+++ src/main/java/_team/earnedit/global/util/EntityFinder.java
@@ -1,12 +1,15 @@
 package _team.earnedit.global.util;
 
+import _team.earnedit.entity.Item;
 import _team.earnedit.entity.Piece;
 import _team.earnedit.entity.User;
 import _team.earnedit.entity.Wish;
 import _team.earnedit.global.ErrorCode;
+import _team.earnedit.global.exception.item.ItemException;
 import _team.earnedit.global.exception.piece.PieceException;
 import _team.earnedit.global.exception.user.UserException;
 import _team.earnedit.global.exception.wish.WishException;
+import _team.earnedit.repository.ItemRepository;
 import _team.earnedit.repository.PieceRepository;
 import _team.earnedit.repository.UserRepository;
 import _team.earnedit.repository.WishRepository;
@@ -20,6 +23,7 @@ public class EntityFinder {
     private final UserRepository userRepository;
     private final WishRepository wishRepository;
     private final PieceRepository pieceRepository;
+    private final ItemRepository itemRepository;
 
     public User getUserOrThrow(Long userId) {
         return userRepository.findById(userId)
@@ -34,5 +38,10 @@ public class EntityFinder {
     public Piece getPieceOrThrow(Long pieceId) {
         return  pieceRepository.findById(pieceId)
                 .orElseThrow(() -> new PieceException(ErrorCode.PIECE_NOT_FOUND));
+    }
+
+    public Item getItemOrThrow(Long itemId) {
+        return itemRepository.findById(itemId)
+                .orElseThrow(() -> new ItemException(ErrorCode.ITEM_NOT_FOUND));
     }
 }
