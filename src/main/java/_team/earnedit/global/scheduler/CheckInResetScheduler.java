d8d61daa4ca92142ad5d340d9a4fdd0f2c4c664b
yoorym-kim <twilight99me@gmail.com>
2025-08-08 15:23:13 +0900

    feat : 불필요 import 삭제

--- src/main/java/_team/earnedit/global/scheduler/CheckInResetScheduler.java
+++ src/main/java/_team/earnedit/global/scheduler/CheckInResetScheduler.java
@@ -1,14 +1,11 @@
 package _team.earnedit.global.scheduler;
 
-import _team.earnedit.entity.User;
 import _team.earnedit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Slf4j
 @Component
