ba9f81416029b27ce39ccf0c2e2670d281d6d4d8
saaad99 <ehdgnstla@naver.com>
2025-08-13 14:36:07 +0900

    ref: 서버측 시각 설정

--- src/main/java/_team/earnedit/EarneditApplication.java
+++ src/main/java/_team/earnedit/EarneditApplication.java
@@ -1,16 +1,30 @@
 package _team.earnedit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableScheduling
+@Slf4j
 public class EarneditApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(EarneditApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void logCurrentTime() {
+		ZonedDateTime now = ZonedDateTime.now();
+		log.info("서버 시작 시각: {}", now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z")));
+	}
 }
