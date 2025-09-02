600790fba99219e8050716fbc7551eb82288892b
saaad99 <ehdgnstla@naver.com>
2025-09-02 11:29:44 +0900

    ref : UserRankInfo 파일 분리

--- src/main/java/_team/earnedit/dto/rank/RankPageResponse.java
+++ src/main/java/_team/earnedit/dto/rank/RankPageResponse.java
@@ -8,17 +8,6 @@ import java.util.List;
 @Getter
 @Builder
 public class RankPageResponse {
-
     private UserRankInfo myRank;
     private List<UserRankInfo> top10;
-
-    @Getter
-    @Builder
-    private static class UserRankInfo {
-        private long userId;
-        private int rank;
-        private String nickname;
-        private long score;
-        private String profileImage;
-    }
 }
