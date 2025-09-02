e546f8c032250f6add9fc237b5a2fced6112f7cb
yoorym-kim <twilight99me@gmail.com>
2025-09-02 19:30:34 +0900

    feat : UserReportRepository 생성

---
+++ src/main/java/_team/earnedit/repository/UserReportRepository.java
@@ -0,0 +1,8 @@
+package _team.earnedit.repository;
+
+import _team.earnedit.entity.UserReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserReportRepository extends JpaRepository<UserReport, Long> {
+
+}
