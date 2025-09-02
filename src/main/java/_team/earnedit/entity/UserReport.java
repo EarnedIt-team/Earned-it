fda2aaf375a301a0dd6df05d161463b502187667
yoorym-kim <twilight99me@gmail.com>
2025-09-02 19:26:28 +0900

    feat : UserReport 엔티티 정의

---
+++ src/main/java/_team/earnedit/entity/UserReport.java
@@ -0,0 +1,39 @@
+package _team.earnedit.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_report")
+@Getter
+@NoArgsConstructor
+public class UserReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reporting_user_id", nullable = false)
+    private User reportingUser; // 신고자
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reported_user_id", nullable = false)
+    private User reportedUser; // 신고 대상
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "reason_id", nullable = false)
+    private ReportReason reason; // 신고 사유
+
+    @Column(name = "reason_text", length = 500)
+    private String reasonText;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+}
