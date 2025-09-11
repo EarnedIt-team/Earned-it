cf1b446f8ef90fe4266a51900095fdfc94a92b7e
saaad99 <ehdgnstla@naver.com>
2025-09-11 18:18:15 +0900

    Revert "09.11 dev -> main merge"

--- src/main/java/_team/earnedit/dto/rank/UserRankInfo.java
+++ src/main/java/_team/earnedit/dto/rank/UserRankInfo.java
@@ -1,19 +1,23 @@
 package _team.earnedit.dto.rank;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserRankInfo {
     private long userId;
     private long rank;
     private String nickname;
     private long score;
     private String profileImage;
-    private boolean isPublic;
+
+    // JPA 네이티브 쿼리 매핑용 생성자
+    public UserRankInfo(long userId, long rank, String nickname, long score, String profileImage) {
+        this.userId = userId;
+        this.rank = rank;
+        this.nickname = nickname;
+        this.score = score;
+        this.profileImage = profileImage;
+    }
 }
