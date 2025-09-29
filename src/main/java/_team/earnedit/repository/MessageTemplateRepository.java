bbf87856ab31aa1f4c148796d0b835c75c92c394
yoorym-kim <twilight99me@gmail.com>
2025-09-29 16:56:46 +0900

    feat : MessageTemplateRepository 생성

---
+++ src/main/java/_team/earnedit/repository/MessageTemplateRepository.java
@@ -0,0 +1,8 @@
+package _team.earnedit.repository;
+
+import _team.earnedit.entity.MessageTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageTemplateRepository extends JpaRepository<MessageTemplate, Long> {
+
+}
