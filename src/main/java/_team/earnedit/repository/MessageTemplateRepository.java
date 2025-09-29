539124c090c6a4e4c0e2a106cf64ea9f01539646
yoorym-kim <twilight99me@gmail.com>
2025-09-29 17:44:55 +0900

    feat : ScheduledNotificationService 스케줄러 구현(sendRandom 메서드에서 FcmService의 sendGlobal 호출 -> 스케줄러 메서드에서 sendRandom 호출)

--- src/main/java/_team/earnedit/repository/MessageTemplateRepository.java
+++ src/main/java/_team/earnedit/repository/MessageTemplateRepository.java
@@ -2,7 +2,8 @@ package _team.earnedit.repository;
 
 import _team.earnedit.entity.MessageTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MessageTemplateRepository extends JpaRepository<MessageTemplate, Long> {
-
+    List<MessageTemplate> findByCategoryAndActiveTrue(MessageTemplate.Category category);
 }
---
+++ src/main/java/_team/earnedit/service/fcm/ScheduledNotificationService.java
@@ -0,0 +1,52 @@
+package _team.earnedit.service.fcm;
+
+import _team.earnedit.entity.MessageTemplate;
+import _team.earnedit.entity.MessageTemplate.Category;
+import _team.earnedit.repository.MessageTemplateRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ScheduledNotificationService {
+
+    private final MessageTemplateRepository templateRepository;
+    private final FcmService fcmService;
+    private final Random rnd = new Random();
+
+    // 출근시간 (09:00 KST)
+    @Scheduled(cron = "0 0 9 * * *")
+    public void sendMorningQuote() {
+        sendRandom(Category.QUOTE);
+    }
+
+    // 점심시간 (12:30 KST)
+    @Scheduled(cron = "0 30 12 * * *")
+    public void sendLunch() {
+        sendRandom(Category.LUNCH);
+    }
+
+    // 퇴근시간 (18:00 KST)
+    @Scheduled(cron = "0 0 18 * * *")
+    public void sendEveningQuote() {
+        sendRandom(Category.QUOTE);
+    }
+
+    // db에 저장된 카테고리별 메시지 중 랜덤 발송
+    private void sendRandom(Category category) {
+        List<MessageTemplate> list = templateRepository.findByCategoryAndActiveTrue(category);
+        if (list.isEmpty()) {
+            log.warn("No message templates for category={}", category);
+            return;
+        }
+        MessageTemplate picked = list.get(rnd.nextInt(list.size()));
+        log.info("Picked message id={} category={}", picked.getId(), category);
+        fcmService.sendGlobal(picked);
+    }
+}
